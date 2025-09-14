b649fcfd2f3c0a4f2999096162cbc18864daec70
Sonu Yadav <sonuyadavkarma@gmail.com>
2025-09-14 17:51:05 +0530

    First Commit

---
+++ Post-Service/Post-Service/src/test/java/com/SonuYadav/Linkedin/Post_Service/PostServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SonuYadav.Linkedin.Post_Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
