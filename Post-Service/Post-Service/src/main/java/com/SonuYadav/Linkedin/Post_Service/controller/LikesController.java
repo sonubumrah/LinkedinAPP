97692c5fd267cd86dd1a830add71fbdadfeff2dd
Sonu Yadav <sonuyadavkarma@gmail.com>
2026-01-24 17:52:36 +0530

    unlike_post_feature_implemented

--- Post-Service/Post-Service/src/main/java/com/SonuYadav/Linkedin/Post_Service/controller/LikesController.java
+++ Post-Service/Post-Service/src/main/java/com/SonuYadav/Linkedin/Post_Service/controller/LikesController.java
@@ -4,10 +4,7 @@ import com.SonuYadav.Linkedin.Post_Service.service.PostLikesService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -20,6 +17,13 @@ public class LikesController {
     public ResponseEntity<Void> likePost(@PathVariable Long postId) {
         log.info("Like post endpoint called");
         postLikesService.likePost(postId,1L);
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("/{postId}")
+    public ResponseEntity<Void> unlikePost(@PathVariable Long postId) {
+        log.info("DisLike post endpoint called");
+        postLikesService.unlikePost(postId,1L);
         return ResponseEntity.noContent().build();
     }
 
